4c9877459182f6bb338aa9d8ee16f0a870edbd62
cr4108 <gosselin.patrick@hydro.qc.ca>
2012-12-05 14:41:23 -0500

    Bug mineur

--- WebContent/scripts/employe.js
+++ WebContent/scripts/employe.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
 		function(event) 
 		{
 			debug = event;
-			if (event.target.id = "bonus")
+			if (event.target.id == "bonus")
 			{
 				//console.log("bonus: ",employes.data[query - 1][event.target.id] , " versus ", (event.target.value == "true"))
 				employes.data[query - 1][event.target.id]  = (event.target.value == "true") ;
@@ -31,6 +31,15 @@ $(document).ready(function(){
 	$('input').change(
 		function(event) 
 		{
+			//console.log("Query: " + query)
+			if(query == null)
+			{
+				//console.log("create new");
+				var newEmploye = createEmploye();
+				employes.data[employes.data.length] = newEmploye;
+				query = employes.data.length
+			}
+			
 			//console.log(event.target.id)
 			if ( ["interne","externe","temporaire"].indexOf(event.target.id) >= 0)
 			{
@@ -53,7 +62,8 @@ $(document).ready(function(){
 				employes.data[query - 1][event.target.id] = event.target.value;
 				debug = event
 				DataSync();
-			}
+			}	
+					
 			localStorage.setItem("employeJS_ind",JSON.stringify(employes));
 		}
 	);
@@ -84,6 +94,24 @@ $( document ).live( 'pageinit',function(event)
 	);
 });
 
+function createEmploye()
+{
+	var properties = {nom:"", email:"", casque:false, territoire:"Québec", typeEmploye:"Interne",inspPrevues:0,
+			salaire:0, inspDelta:0, bonus:false, bottes:false, lunettes:false, remarque:"", telephone:""};
+	
+	var employe = {attached:true, nom:"", email:"", casque:false, territoire:"Québec", typeEmploye:"Interne",inspPrevues:0,
+			salaire:0, inspDelta:0, bonus:false, bottes:false, lunettes:false, remarque:"", telephone:"", properties:properties};
+	
+	return employe;
+}
+
+function deleteEmpl(e)
+{
+	//console.log(e)//, " - ", $li.context.id, " -> ",$('#liste').children());
+
+	$('#liste').children().remove("li#"+e);
+	$('#liste').listview('refresh');
+}
 function DataCtrl()//$scope)
 {
 	var value;
@@ -124,35 +152,67 @@ function DataSync()
 {
 	var index = query - 1;
 	const pos = "employeJS_ind";
-		
-	var jr = JSON.stringify(employes.data[query-1]);	
-	var myUrl = localStorage.getItem('url').concat("/api/employe/", employes.data[query-1].id);
-
-	jr = "".concat("{" + '"success"' +  ":true,"+ '"data"' + ":", jr, "}");
-	//console.log("jr!: ",jr);
-	//console.log("url: ",myUrl);
 	
-    $.ajax(
-            {
-            	type: "PUT",
-            	url: myUrl,
-            	data: jr,
-                success: function (i)
-                {
-                	//console.log("success sync");
-                },
-               
-                error: function (jqXHR,textStatus,errorThrown)
-                {
-                	debug = jqXHR;
-                    //console.log("fail ",jqXHR);
-                    //console.log("status ",textStatus);
-                    //console.log("err ",errorThrown);
-                }
-            } // fin argument ajax
-       );//Fin Ajax
-       
-}//Fin DataCtrl
+	var jr = JSON.stringify(employes.data[query-1]);	
+	
+	if(employes.data[query-1].id != null)
+	{
+		var myUrl = localStorage.getItem('url').concat("/api/employe/", employes.data[query-1].id);
+	
+		jr = "".concat("{" + '"success"' +  ":true,"+ '"data"' + ":", jr, "}");
+		//console.log("jr!: ",jr);
+		//console.log("url: ",myUrl);
+		
+	    $.ajax(
+	            {
+	            	type: "PUT",
+	            	url: myUrl,
+	            	data: jr,
+	                success: function (i)
+	                {
+	                	//console.log("success sync");
+	                },
+	               
+	                error: function (jqXHR,textStatus,errorThrown)
+	                {
+	                	debug = jqXHR;
+	                    //console.log("fail ",jqXHR);
+	                    //console.log("status ",textStatus);
+	                    //console.log("err ",errorThrown);
+	                }
+	            } // fin argument ajax
+	       );//Fin Ajax
+	}
+	else
+	{	
+		var myUrl = localStorage.getItem('url').concat("/api/employe");
+	
+		jr = "".concat("{" + '"success"' +  ":true,"+ '"data"' + ":", jr, "}");
+		//console.log("jr!: ",jr);
+		//console.log("url: ",myUrl);
+		
+	    $.ajax(
+	            {
+	            	type: "POST",
+	            	url: myUrl,
+	            	data: jr,
+	                success: function (i)
+	                {
+	                	//console.log("success sync");
+	                	console.log(i);
+	                },
+	               
+	                error: function (jqXHR,textStatus,errorThrown)
+	                {
+	                	debug = jqXHR;
+	                    //console.log("fail ",jqXHR);
+	                    //console.log("status ",textStatus);
+	                    //console.log("err ",errorThrown);
+	                }
+	            } // fin argument ajax
+	       );//Fin Ajax
+	}
+}//Fin
 
 function InspectionCtrl($scope) 
 {
@@ -247,69 +307,92 @@ var query ;
 
 $("#employeDetail").live("pageshow", function(e, data) 
 	{
-		//var query = "id=";
-
 		query = data.prevPage.context.URL.split("?")[1];
-		query = query.replace("id=","");
-		//console.log("qu: ", query);
-		for (var i=0; i<employes.data.length; i++)
+		if(query != null)
 		{
-			//console.log("        employes.data[i].id: ",employes.data[i].id, " ",
-			//		" employes.data[i].nom ",employes.data[i].nom 
-			//)
-					
-			if (employes.data[i].id == query)
-				{
-					//console.log("   ",i+1)
-					query = (i + 1)
-				}
-		}
-		//query is now an ID, do stuff with it... test
-		document.getElementById("titre").innerHTML= employes.data[query-1].nom;
-		document.getElementById("nom").value = employes.data[query-1].nom;
-		document.getElementById("email").value = employes.data[query-1].email;
-		document.getElementById("telephone").value = employes.data[query-1].telephone;
-		document.getElementById("remarque").value = employes.data[query-1].remarque;
-		document.getElementById("bonus").value = employes.data[query-1].bonus;
+			query = query.replace("id=","");
+			//console.log("qu: ", query);
+			for (var i=0; i<employes.data.length; i++)
+			{
+				//console.log("        employes.data[i].id: ",employes.data[i].id, " ",
+				//		" employes.data[i].nom ",employes.data[i].nom 
+				//)
+						
+				if (employes.data[i].id == query)
+					{
+						//console.log("   ",i+1)
+						query = (i + 1)
+					}
+			}
+			//query is now an ID, do stuff with it... test
+			document.getElementById("titre").innerHTML= employes.data[query-1].nom;
+			document.getElementById("nom").value = employes.data[query-1].nom;
+			document.getElementById("email").value = employes.data[query-1].email;
+			document.getElementById("telephone").value = employes.data[query-1].telephone;
+			document.getElementById("remarque").value = employes.data[query-1].remarque;
+			document.getElementById("bonus").value = employes.data[query-1].bonus;
+			
+			switch (employes.data[query-1].typeEmploye.toLowerCase())
+			{
+				case "interne":
+					document.getElementById("interne").checked = true;
+					$('#externe').checkboxradio ("refresh");
+					$('#interne').checkboxradio ("refresh");
+					$('#temporaire').checkboxradio ("refresh");
+					break;
+				case "externe":
+					document.getElementById("externe").checked = true;
+					$('#externe').checkboxradio ("refresh");
+					$('#interne').checkboxradio ("refresh");
+					$('#temporaire').checkboxradio ("refresh");
+					break;
+				case "temporaire":
+					document.getElementById("temporaire").checked = true;
+					$('#externe').checkboxradio ("refresh");
+					$('#interne').checkboxradio ("refresh");
+					$('#temporaire').checkboxradio ("refresh");
+					break;
+			}
 		
-		switch (employes.data[query-1].typeEmploye.toLowerCase())
+			document.getElementById("salaire").value = employes.data[query-1].salaire;
+			
+			//console.log("query: ",employes.data[query-1].nom, " ", employes.data[query-1].casque);
+			document.getElementById("casque").checked = employes.data[query-1].casque ;
+			$('#casque').checkboxradio ("refresh");
+			
+			//console.log("query: ",employes.data[query-1].nom, " ", employes.data[query-1].lunettes);
+			document.getElementById("lunettes").checked = employes.data[query-1].lunettes ;
+			$('#lunette').checkboxradio("refresh");
+			
+			//console.log("query: ",employes.data[query-1].nom, " ", employes.data[query-1].bottes);
+			document.getElementById("bottes").checked = employes.data[query-1].bottes ;
+			$('#bottes').checkboxradio ("refresh");
+			
+			document.getElementById("territoire").value = employes.data[query-1].territoire;
+			$('#territoire').selectMenu;
+		}
+		else
 		{
-			case "interne":
-				document.getElementById("interne").checked = true;
-				$('#externe').checkboxradio ("refresh");
-				$('#interne').checkboxradio ("refresh");
-				$('#temporaire').checkboxradio ("refresh");
-				break;
-			case "externe":
-				document.getElementById("externe").checked = true;
-				$('#externe').checkboxradio ("refresh");
-				$('#interne').checkboxradio ("refresh");
-				$('#temporaire').checkboxradio ("refresh");
-				break;
-			case "temporaire":
-				document.getElementById("temporaire").checked = true;
-				$('#externe').checkboxradio ("refresh");
-				$('#interne').checkboxradio ("refresh");
-				$('#temporaire').checkboxradio ("refresh");
-				break;
+			document.getElementById("titre").innerHTML= "";
+			document.getElementById("nom").value = "";
+			document.getElementById("email").value = "";
+			document.getElementById("telephone").value = "";
+			document.getElementById("remarque").value = "";
+			document.getElementById("bonus").value = "";
+			document.getElementById("interne").checked = true
+			$('#externe').checkboxradio ("refresh");
+			$('#interne').checkboxradio ("refresh");
+			$('#temporaire').checkboxradio ("refresh");
+			document.getElementById("salaire").value = "0";
+			document.getElementById("casque").checked = false ;
+			$('#casque').checkboxradio ("refresh");
+			document.getElementById("lunettes").checked = false;
+			$('#lunettes').checkboxradio("refresh");
+			document.getElementById("bottes").checked = false ;
+			$('#bottes').checkboxradio ("refresh");
+			document.getElementById("territoire").value = "Québec";
+			$('#territoire').selectMenu;
 		}
-	
-		document.getElementById("salaire").value = employes.data[query-1].salaire;
-		
-		//console.log("query: ",employes.data[query-1].nom, " ", employes.data[query-1].casque);
-		document.getElementById("casque").checked = employes.data[query-1].casque ;
-		$('#casque').checkboxradio ("refresh");
-		
-		//console.log("query: ",employes.data[query-1].nom, " ", employes.data[query-1].lunettes);
-		document.getElementById("lunettes").checked = employes.data[query-1].lunettes ;
-		$('#lunette').checkboxradio("refresh");
-		
-		//console.log("query: ",employes.data[query-1].nom, " ", employes.data[query-1].bottes);
-		document.getElementById("bottes").checked = employes.data[query-1].bottes ;
-		$('#bottes').checkboxradio ("refresh");
-		
-		document.getElementById("territoire").value = employes.data[query-1].territoire;
-		$('#territoire').selectMenu;
 	}
 );
 
@@ -319,6 +402,25 @@ $("#config").live("pageshow", function(e, data)
 			document.getElementById('popupText').innerHTML = checkConnection();	
 		}
 		);
+
+$("#suivi").live("pageshow", function(e, data) 
+		{
+			
+		}
+);
+
+function refreshPage() 
+{
+	console.log("IN refreshPage");
+	$.mobile.changePage(window.location.href,
+			{
+		allowSamePageTransition : true,
+		transition              : 'none',
+		showLoadMsg             : false,
+		reloadPage              : true
+			}
+		);
+}
 
 function checkConnection()
 {
@@ -354,4 +456,4 @@ function updateUrl(value)
 {
 	localStorage.setItem('url', value);
 }
-//----END PAGE
+
